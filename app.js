435e916a873d8be189f506a72c35b3d8eeb55b63
ally-petitt <allypetitt@gmail.com>
2021-03-09 09:33:35 -0800

    added images

--- app.js
+++
@@ -1,7 +0,0 @@
-var controller = new ScrollMagic.Controller();
-
-var scene = new ScrollMagic.Scene({
-  triggerElement: ".projects",
-})
-  .setClassToggle(".proj-1-img", "scaleUp")
-  .addTo(controller);
